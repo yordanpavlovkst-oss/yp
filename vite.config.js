09b3d0d21f14c3205d6ed09f56dd0a853d098f7c
yordanpavlovkst-oss <yordanpavlovkst@gmail.com>
2025-09-06 19:09:27 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
+
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins:[react()] });
+export default defineConfig({
+  plugins: [react()],
+  base: './'
+});
